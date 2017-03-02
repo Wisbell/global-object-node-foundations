4b952b246272ed570cffef66f7c16ba2dcbd193a
Wisbell <westleyisbell@gmail.com>
2017-03-02 14:21:49 -0600

    Exercise now complete.  Used object destructuring to grab necessary values and used stdout.write to display them instead of console.log

---
+++ global.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+// chmod +x direct-exec.js
+
+'use strict';
+
+let {node, v8} = process.versions
+
+// console.log(node)
+// console.log(v8)
+
+process.stdout.write(`Node.js version: ${node}\n`)
+process.stdout.write(`V8 version: ${v8}\n`)
